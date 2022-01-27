c4d336df77356d8db42205756cbbba50a11c4efc
lizipeng.wy <lizipeng.wy@bytedance.com>
2022-01-27 12:32:09 +0800

    feat: TODO 音程答案

--- src/pages/Interval/index.jsx
+++ src/pages/Interval/index.jsx
@@ -22,76 +22,30 @@ export default function Interval() {
   }
 
   // 标记 -1:降, 0:原, 1:升
-  const genInterval = async () => {
-    const getComputedVal = (xNode) => {
-      const { adj, note } = xNode || {};
-      let val = note;
-      switch (adj) {
-        case 0:
-          return val;
-        case 1:
-          if (note === 3) {
-            return val + 1;
-          }
-          if (note === 7) {
-            return 1;
-          }
-          return val + 0.5;
-        case -1:
-          if (note === 4) {
-            return val - 1;
-          }
-          if (note === 1) {
-            return 7;
-          }
-          return val - 0.5;
-      }
-    };
+  const genInterval = () => {
     const aNote = {
       adj: getRandomIntByRange(-1, 1),
       note: getRandomIntByRange(1, 7),
     };
-    await new Promise((resolve) => setTimeout(resolve, 100));
     const bNote = {
       adj: getRandomIntByRange(-1, 1),
       note: getRandomIntByRange(1, 7),
     };
-
-    console.log('aNote==>', aNote);
-    console.log('bNote==>', bNote);
-    
-    /**
-     * 计算出用于比较的值: 约定升号 +0.5, 降号 -0.5
-     * 注意:
-     * 1) 3-4, 7-1, 经过这两组音, +-0.5 变为 +-1
-     * 2) 做加法时对 7 取模, 做减法时遇到 0 变为 7
-     */
-    // const aComputedVal = getComputedVal(aNote);
-    // const bComputedVal = getComputedVal(bNote);
-
-    // console.log('aVal==>', aComputedVal);
-    // console.log('bVal==>', bComputedVal);
-
-    // if (aComputedVal > bComputedVal || (aComputedVal === bComputedVal && aNote.note > bNote.note)) {
-    //   return [bNote, aNote];
-    // }
     return [aNote, bNote];
   }
 
   // 计算音程关系
   const getIntervalDist = () => {
-    if (!interval) {
-      return '-';
-    }
-
+    // TODO
+    return '敬请期待';
   }
 
-  const updateInterval = async () => {
+  const updateInterval = () => {
     wx.showLoading({ title: '获取中' });
     setTimeout(() => {
       wx.hideLoading();
     }, 200);
-    const interval = await genInterval();
+    const interval = genInterval();
     setInterval(interval);
   }
 
@@ -100,7 +54,7 @@ export default function Interval() {
       showCancel: false,
       confirmText: '我知道了',
       title: computedInterval(),
-      content: `音程关系: `,
+      content: getIntervalDist(),
       confirmColor: '#6190e8',
     });
   }
